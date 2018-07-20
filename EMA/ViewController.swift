106cb432c64078fe90acbe72cb5681fbfe83d8a5
Mustafa Sahinli <mustafasahinli@mustafas-air>
2018-07-20 12:09:38 +0200

    Initial Commit

---
+++ EMA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EMA
+//
+//  Created by Mustafa Sahinli on 20.07.18.
+//  Copyright © 2018 Mustafa Sahinli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
