21c4b4513b9a042d01b2fc09a26d8f66308fe0aa
Mustafa Sahinli <mustafasahinli@p200300c6272336010853d49ea2c3da93.dip0.t-ipconnect.de>
2018-07-20 15:28:15 +0200

    - Secure Text Feld wurde aktiviert. - Implementation der Funktion für den Abruf des Benutzernamen auf der App und des Passwortes. - Theoretische Ausgabe der Fehlermeldung als Print und dem erfolgreichen Login beim Klicken auf LOGIN!

--- EMA/ViewController.swift
+++ EMA/ViewController.swift
@@ -12,8 +12,34 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
-
+    @IBOutlet weak var username: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    @IBAction func login(_ sender: Any) {
+        
+        
+        let benutzername = self.username.text
+        let passwort = self.password.text
+        let db_benutzername = "benutzername"
+        let db_passwort = "passwort"
+        if benutzername == db_benutzername && passwort == db_passwort
+        {
+          
+            print("Test")
+        }
+        else  {
+            print ("Fehlgeschlagen")
+        }
+        //Methode für das Überprüfen vom Passwort und dem Benutzernamen und Action Button für das Login
+        
+    }
+    
+            
+        
+        
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
