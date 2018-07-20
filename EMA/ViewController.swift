d76c82c707ead1b273632c17bfbee72136d17a39
Mustafa Sahinli <mustafasahinli@mustafas-air>
2018-07-20 14:31:05 +0200

    second commit

--- EMA/ViewController.swift
+++ EMA/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Test s
+        // Test ss
     }
 
     override func didReceiveMemoryWarning() {
