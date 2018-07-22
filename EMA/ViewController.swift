6e447ecc3c13e395ee7da6c5bc033006cd17e71a
Ertugrul Yilmaz <ertugrul.yilmaz@stud.th-bingen.de>
2018-07-22 19:11:38 +0200

    Show/Hide button for the password textfield added

--- EMA/ViewController.swift
+++ EMA/ViewController.swift
@@ -10,12 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+    var isPasswordVisible = false
+
+    @IBOutlet weak var showHideButton: UIButton!
     @IBOutlet weak var errorcode: UILabel!
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var password: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        showHideButton.setImage(#imageLiteral(resourceName: "hide"), for: .normal)
+    }
+
     @IBAction func login(_ sender: Any) {
 
         let loginManager = LoginManager()
@@ -23,14 +28,34 @@ class ViewController: UIViewController {
         let isLoginSuccessful = loginManager.tryLogin(username: self.username.text!, password: self.password.text!)
 
         if isLoginSuccessful {
-            print("Erfolgreich")
             self.errorcode.text = ""
         } else {
-            print ("Fehlgeschlagen")
+            fadeViewIn(view: errorcode)
             self.errorcode.text = "Authentifizierung fehlgeschlagen"
         }
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+
+    func fadeViewIn(view: UIView) {
+        let animationDuration = 0.75
+        view.alpha = 0
+        UIView.animate(withDuration: animationDuration, animations: { () -> Void in
+            view.alpha = 1
+        })
+    }
+    @IBAction func showHideButtonTouchUpInsideHandler(_ sender: Any) {
+        isPasswordVisible = !isPasswordVisible
+        if isPasswordVisible {
+            password.isSecureTextEntry = false
+            showHideButton.setImage(#imageLiteral(resourceName: "show"), for: .normal)
+        } else {
+            password.isSecureTextEntry = true
+            showHideButton.setImage(#imageLiteral(resourceName: "hide"), for: .normal)
+        }
+        let currentText: String = password.text!
+        password.text = ""
+        password.text = currentText
     }
 }
