c9fac900e3ab10f0cb77e2d7c2cddaebabb3902d
Veli Tasyurdu <veli@velis-mbp.home>
2018-08-01 16:46:47 +0200

    - save fields to database

--- EMA/DatabaseModels/Field.swift
+++ EMA/DatabaseModels/Field.swift
@@ -11,4 +11,12 @@ import RealmSwift
 
 class Field: Object {
     @objc dynamic var name = ""
+    @objc dynamic var sort = ""
+    @objc dynamic var size = 0.0
+    let position = List<Location>()
+}
+
+class Location: Object {
+    @objc var latitude = 0.0
+    @objc var longitude = 0.0
 }
--- EMA/Fields/MapViewController.swift
+++ EMA/Fields/MapViewController.swift
@@ -9,13 +9,15 @@
 import UIKit
 import MapKit
 import GLKit
+import RealmSwift
 
 class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    var fields: Results<Object>?
     var canDrawField: Bool = false {
         didSet {
             if canDrawField {
                 self.navigationController?.navigationBar.topItem?.title = "Feld anlegen"
-                let saveButton = UIBarButtonItem(image: UIImage(named: "check"), style: .plain, target: self, action: nil)
+                let saveButton = UIBarButtonItem(image: UIImage(named: "check"), style: .plain, target: self, action: #selector(saveField))
                 self.navigationController?.navigationBar.topItem?.rightBarButtonItem = saveButton
             } else {
                 self.navigationController?.navigationBar.topItem?.title = "Karte"
@@ -28,6 +30,8 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     var points = [CLLocationCoordinate2D]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.fields = DatabaseManager.shared.getObjects(type: Field.self)
+        drawFieldsOnMap()
         self.mapView.delegate = self
     }
     override func viewWillAppear(_ animated: Bool) {
@@ -72,6 +76,63 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         }
         return MKPolylineRenderer(overlay: overlay)
     }
+    @objc private func saveField() {
+        if points.count < 3 {
+            alert(message: "Lege min. 3 Punkte an")
+        } else {
+            let alertController = UIAlertController(title: "Feld speichern", message: nil, preferredStyle: .alert)
+            alertController.addTextField { (textField) in
+                textField.placeholder = "Feldname"
+            }
+            alertController.addTextField { (textField) in
+                textField.placeholder = "Rebensorte"
+            }
+            let saveAction = UIAlertAction(title: "Speichern", style: .default) { (alert) in
+                let nameTextField = alertController.textFields![0] as UITextField
+                let sortTextField = alertController.textFields![1] as UITextField
+                self.saveToDB(name: nameTextField.text!, sort: sortTextField.text!)
+            }
+            let cancelAction = UIAlertAction(title: "Abbrechen", style: .cancel, handler: nil)
+            alertController.addAction(saveAction)
+            alertController.addAction(cancelAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+    @objc private func saveToDB(name: String, sort: String) {
+        if !name.isEmpty || !sort.isEmpty {
+            let field: Field = Field()
+            field.name = name
+            field.sort = sort
+            field.size = getSizeOfField()
+            for point in points {
+                let location = Location()
+                location.latitude = Double(point.latitude)
+                location.longitude = Double(point.longitude)
+                field.position.append(location)
+            }
+            DatabaseManager.shared.addToDatabase(object: field)
+            self.canDrawField = false
+        } else {
+            alert(message: "Feldname oder Reben Sorte darf nicht leer sein")
+        }
+    }
+    private func drawFieldsOnMap() {
+        var fieldPoints = [CLLocationCoordinate2D]()
+        for field in fields! {
+            guard let field = field as? Field else { return }
+            for position in field.position {
+                let lat = position.latitude
+                let long = position.longitude
+                print(position.latitude)
+                let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+                fieldPoints.append(coordinate)
+                print(fieldPoints)
+                let polygon = MKPolygon(coordinates: &fieldPoints, count: fieldPoints.count)
+                self.mapView.add(polygon)
+                fieldPoints.removeAll()
+            }
+        }
+    }
     private func getCenterOfField() -> CLLocationCoordinate2D {
         var xVal: Float = 0.0
         var yVal: Float = 0.0
@@ -107,6 +168,12 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         }
         area = -(area * kEarthRadius * kEarthRadius / 2)
         return max(area, -area)
+    }
+    private func alert(message: String) {
+        let alert = UIAlertController(title: message, message: nil, preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "Schließen", style: .cancel, handler: nil)
+        alert.addAction(cancel)
+        self.present(alert, animated: true, completion: nil)
     }
 
 }
