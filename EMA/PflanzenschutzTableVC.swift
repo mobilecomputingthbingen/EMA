9249dad6103dd9e777965228f028976ca1d4d80b
Veli Tasyurdu <veli@velis-mbp.home>
2018-08-03 21:42:57 +0200

    - added Controls for Pflanzenschutz

--- EMA/PflanzenschutzTableVC.swift
+++ EMA/PflanzenschutzTableVC.swift
@@ -1,5 +1,5 @@
 //
-//  PflanzenschutzTableVCTableViewController.swift
+//  PflanzenschutzTableVC.swift
 //  EMA
 //
 //  Created by Veli Tasyurdu on 03.08.18.
@@ -9,29 +9,29 @@
 import UIKit
 
 class PflanzenschutzTableVC: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         let addButton = UIBarButtonItem(image: UIImage(named: "add"), style: .plain, target: self, action: #selector(add))
         self.navigationItem.rightBarButtonItem = addButton
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return 0
@@ -45,5 +45,50 @@ class PflanzenschutzTableVC: UITableViewController {
         workRoutineNormalViewController?.navigationItem.backBarButtonItem?.title = "Zurück"
         self.navigationController?.pushViewController(workRoutineNormalViewController!, animated: true)
     }
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
--- EMA/PflanzenschutzViewController.swift
+++ EMA/PflanzenschutzViewController.swift
@@ -8,17 +8,185 @@
 
 import UIKit
 
-class PflanzenschutzViewController: UIViewController {
+class PflanzenschutzViewController: UIViewController, UIPickerViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var mainView: UIStackView!
+    let dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.textColor = .black
+        label.text = "Datum"
+        return label
+    }()
+    lazy var datePicker: UIDatePicker = {
+        let picker = UIDatePicker()
+        picker.datePickerMode = .date
+        return picker
+    }()
+    let fieldLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.textColor = .black
+        label.text = "Feld"
+        return label
+    }()
+    lazy var fieldPicker: UIPickerView = {
+        let picker = UIPickerView()
+        picker.delegate = self
+        return picker
+    }()
+    let userLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.textColor = .black
+        label.text = "Benutzer"
+        return label
+    }()
+    let userTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .line
+        textField.text = "benutzername"
+        return textField
+    }()
+    let workingHourLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.textColor = .black
+        label.text = "Arbeitszeit (Stunden)"
+        return label
+    }()
+    lazy var workingHourPicker: UIPickerView = {
+        let picker = UIPickerView()
+        picker.delegate = self
+        return picker
+    }()
+    let categoryLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.textColor = .black
+        label.text = "Pflantenschutzmittel Kategorie"
+        return label
+    }()
+    lazy var categoryPicker: UIPickerView = {
+        let picker = UIPickerView()
+        picker.delegate = self
+        return picker
+    }()
+    let againstLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.textColor = .black
+        label.text = "Pflantenschutzmittel Kategorie"
+        return label
+    }()
+    let againstTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .line
+        textField.text = "benutzername"
+        return textField
+    }()
+    let protectionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.textColor = .black
+        label.text = "Pflantenschutzmittel Kategorie"
+        return label
+    }()
+    let protectionTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .line
+        textField.text = "benutzername"
+        return textField
+    }()
+    let dueLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.textColor = .black
+        label.text = "Pflantenschutzmittel Kategorie"
+        return label
+    }()
+    let dueTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .line
+        textField.text = "benutzername"
+        return textField
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setup()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func setup() {
+        mainView.addSubview(dateLabel)
+        dateLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: dateLabel, attribute: .left, relatedBy: .equal, toItem: mainView, attribute: .left, multiplier: 1.0, constant: 16).isActive = true  // swiftlint:disable:this line_length
+        NSLayoutConstraint(item: dateLabel, attribute: .top, relatedBy: .equal, toItem: mainView, attribute: .top, multiplier: 1.0, constant: 16).isActive = true  // swiftlint:disable:this line_length
+        mainView.addSubview(datePicker)
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: datePicker, attribute: .left, relatedBy: .equal, toItem: mainView, attribute: .left, multiplier: 1.0, constant: 16).isActive = true  // swiftlint:disable:this line_length
+        NSLayoutConstraint(item: datePicker, attribute: .top, relatedBy: .equal, toItem: dateLabel, attribute: .bottom, multiplier: 1.0, constant: 8).isActive = true  // swiftlint:disable:this line_length
+        mainView.addSubview(fieldLabel)
+        fieldLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: fieldLabel, attribute: .left, relatedBy: .equal, toItem: mainView, attribute: .left, multiplier: 1.0, constant: 16).isActive = true  // swiftlint:disable:this line_length
+        NSLayoutConstraint(item: fieldLabel, attribute: .top, relatedBy: .equal, toItem: datePicker, attribute: .bottom, multiplier: 1.0, constant: 16).isActive = true  // swiftlint:disable:this line_length
+        mainView.addSubview(fieldPicker)
+        fieldPicker.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: fieldPicker, attribute: .left, relatedBy: .equal, toItem: mainView, attribute: .left, multiplier: 1.0, constant: 16).isActive = true  // swiftlint:disable:this line_length
+        NSLayoutConstraint(item: fieldPicker, attribute: .top, relatedBy: .equal, toItem: fieldLabel, attribute: .bottom, multiplier: 1.0, constant: 8).isActive = true  // swiftlint:disable:this line_length
+        mainView.addSubview(userLabel)
+        userLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: userLabel, attribute: .left, relatedBy: .equal, toItem: mainView, attribute: .left, multiplier: 1.0, constant: 16).isActive = true  // swiftlint:disable:this line_length
+        NSLayoutConstraint(item: userLabel, attribute: .top, relatedBy: .equal, toItem: fieldPicker, attribute: .bottom, multiplier: 1.0, constant: 16).isActive = true  // swiftlint:disable:this line_length
+        mainView.addSubview(userTextField)
+        userTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: userTextField, attribute: .left, relatedBy: .equal, toItem: mainView, attribute: .left, multiplier: 1.0, constant: 16).isActive = true  // swiftlint:disable:this line_length
+        NSLayoutConstraint(item: userTextField, attribute: .top, relatedBy: .equal, toItem: userLabel, attribute: .bottom, multiplier: 1.0, constant: 16).isActive = true  // swiftlint:disable:this line_length
+        mainView.addSubview(workingHourLabel)
+        workingHourLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: workingHourLabel, attribute: .left, relatedBy: .equal, toItem: mainView, attribute: .left, multiplier: 1.0, constant: 16).isActive = true  // swiftlint:disable:this line_length
+        NSLayoutConstraint(item: workingHourLabel, attribute: .top, relatedBy: .equal, toItem: userTextField, attribute: .bottom, multiplier: 1.0, constant: 16).isActive = true  // swiftlint:disable:this line_length
+        mainView.addSubview(workingHourPicker)
+        workingHourPicker.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: workingHourPicker, attribute: .left, relatedBy: .equal, toItem: mainView, attribute: .left, multiplier: 1.0, constant: 16).isActive = true  // swiftlint:disable:this line_length
+        NSLayoutConstraint(item: workingHourPicker, attribute: .top, relatedBy: .equal, toItem: workingHourLabel, attribute: .bottom, multiplier: 1.0, constant: 8).isActive = true  // swiftlint:disable:this line_length
+        mainView.addSubview(categoryLabel)
+        categoryLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: categoryLabel, attribute: .left, relatedBy: .equal, toItem: mainView, attribute: .left, multiplier: 1.0, constant: 16).isActive = true  // swiftlint:disable:this line_length
+        NSLayoutConstraint(item: categoryLabel, attribute: .top, relatedBy: .equal, toItem: workingHourPicker, attribute: .bottom, multiplier: 1.0, constant: 16).isActive = true  // swiftlint:disable:this line_length
+        mainView.addSubview(categoryPicker)
+        categoryPicker.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: categoryPicker, attribute: .left, relatedBy: .equal, toItem: mainView, attribute: .left, multiplier: 1.0, constant: 16).isActive = true   // swiftlint:disable:this line_length
+        NSLayoutConstraint(item: categoryPicker, attribute: .top, relatedBy: .equal, toItem: categoryLabel, attribute: .bottom, multiplier: 1.0, constant: 8).isActive = true  // swiftlint:disable:this line_length
+        mainView.addSubview(againstLabel)
+        againstLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: againstLabel, attribute: .left, relatedBy: .equal, toItem: mainView, attribute: .left, multiplier: 1.0, constant: 16).isActive = true  // swiftlint:disable:this line_length
+        NSLayoutConstraint(item: againstLabel, attribute: .top, relatedBy: .equal, toItem: categoryPicker, attribute: .bottom, multiplier: 1.0, constant: 16).isActive = true  // swiftlint:disable:this line_length
+        mainView.addSubview(againstTextField)
+        againstTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: againstTextField, attribute: .left, relatedBy: .equal, toItem: mainView, attribute: .left, multiplier: 1.0, constant: 16).isActive = true   // swiftlint:disable:this line_length
+        NSLayoutConstraint(item: againstTextField, attribute: .top, relatedBy: .equal, toItem: againstLabel, attribute: .bottom, multiplier: 1.0, constant: 8).isActive = true  // swiftlint:disable:this line_length
+        mainView.addSubview(protectionLabel)
+        protectionLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: protectionLabel, attribute: .left, relatedBy: .equal, toItem: mainView, attribute: .left, multiplier: 1.0, constant: 16).isActive = true  // swiftlint:disable:this line_length
+        NSLayoutConstraint(item: protectionLabel, attribute: .top, relatedBy: .equal, toItem: againstTextField, attribute: .bottom, multiplier: 1.0, constant: 16).isActive = true  // swiftlint:disable:this line_length
+        mainView.addSubview(protectionTextField)
+        protectionTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: protectionTextField, attribute: .left, relatedBy: .equal, toItem: mainView, attribute: .left, multiplier: 1.0, constant: 16).isActive = true   // swiftlint:disable:this line_length
+        NSLayoutConstraint(item: protectionTextField, attribute: .top, relatedBy: .equal, toItem: protectionLabel, attribute: .bottom, multiplier: 1.0, constant: 8).isActive = true  // swiftlint:disable:this line_length
+        mainView.addSubview(dueLabel)
+        dueLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: dueLabel, attribute: .left, relatedBy: .equal, toItem: mainView, attribute: .left, multiplier: 1.0, constant: 16).isActive = true  // swiftlint:disable:this line_length
+        NSLayoutConstraint(item: dueLabel, attribute: .top, relatedBy: .equal, toItem: protectionTextField, attribute: .bottom, multiplier: 1.0, constant: 16).isActive = true  // swiftlint:disable:this line_length
+        mainView.addSubview(dueTextField)
+        dueTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: dueTextField, attribute: .left, relatedBy: .equal, toItem: mainView, attribute: .left, multiplier: 1.0, constant: 16).isActive = true   // swiftlint:disable:this line_length
+        NSLayoutConstraint(item: dueTextField, attribute: .top, relatedBy: .equal, toItem: dueLabel, attribute: .bottom, multiplier: 1.0, constant: 8).isActive = true  // swiftlint:disable:this line_length
     }
-
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView == fieldPicker {
+            return "Hello"
+        }
+        return nil
+    }
 }
